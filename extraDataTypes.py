7f46233409e96fd42bd27c61f741c32133327072
zain-23 <zaingit780@gmail.com>
2025-05-08 17:37:46 +0500

    Add FastAPI application with root endpoint and item update functionality

---
+++ extraDataTypes.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Body
+from uuid import UUID
+from datetime import datetime, timedelta, time
+from typing import Annotated
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message":"Hello world"}
+
+@app.put("/items/{item_id}")
+def update_item(
+    item_id: UUID,
+    start_datetime: Annotated[datetime, Body()],
+    end_datetime: Annotated[datetime, Body()],
+    process_after: Annotated[timedelta, Body()],
+    repeat_at: Annotated[time | None, Body()] = None,
+):
+    start_process = start_datetime + process_after
+    duration = end_datetime - start_process
+    return {
+        "item_id": item_id,
+        "start_datetime": start_datetime,
+        "end_datetime": end_datetime,
+        "process_after": process_after,
+        "repeat_at": repeat_at,
+        "start_process": start_process,
+        "duration": duration,
+    }
+    
