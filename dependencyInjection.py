1c4c08b235109eecc88d490dfd9c9659e262bef1
zain-23 <zaingit780@gmail.com>
2025-05-09 16:34:19 +0500

    learn dependency injection

---
+++ dependencyInjection.py
@@ -0,0 +1,20 @@
+from typing import Annotated
+from typing import Union
+from fastapi import Depends, FastAPI
+
+app = FastAPI()
+
+
+async def common_parameters(q:Union[str, None] = None, skip: int = 0, limit: int = 100):
+    print("RUN")
+    return {"q": q, "skip": skip, "limit": limit}
+
+
+@app.get("/items")
+async def read_items(commons: Annotated[dict, Depends(common_parameters)]):
+    return commons
+
+
+@app.get("/users")
+async def read_users(commons: Annotated[dict, Depends(common_parameters)]):
+    return commons
